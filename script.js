265f4881e3f53f0a354cd68a6ff49bf36b59c043
Jawad Khalil <iamjawadkhalil@gmail.com>
2024-05-09 16:49:29 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,22 @@
-function addRecommendation() {
-    // Get the message of the new recommendation
-    let recommendation = document.getElementById("new_recommendation");
-    // If the user has left a recommendation, display a pop-up
-    if (recommendation.value != null && recommendation.value.trim() != "") {
-      console.log("New recommendation added");
-      //Call showPopup here
-      showPopup(true);
-      // Create a new 'recommendation' element and set it's value to the user's message
-      var element = document.createElement("div");
-      element.setAttribute("class","recommendation");
-      element.innerHTML = "\<span\>&#8220;\</span\>" + recommendation.value + "\<span\>&#8221;\</span\>";
-      // Add this element to the end of the list of recommendations
-      document.getElementById("all_recommendations").appendChild(element); 
-      
-      // Reset the value of the textarea
-      recommendation.value = "";
-    }
-  }
-  
-  function showPopup(bool) {
-    if (bool) {
-      document.getElementById('popup').style.visibility = 'visible'
+function toggleMenu() {
+  const menu = document.getElementById('menu');
+  menu.classList.toggle('active');
+}
+
+window.addEventListener("scroll", function () {
+    var scrollBtn = document.getElementById("scrollToTopBtn");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollBtn.style.display = "block";
     } else {
-      document.getElementById('popup').style.visibility = 'hidden'
+        scrollBtn.style.display = "none";
     }
-  }
-  
+});
+
+function scrollToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+document.getElementById("scrollToTopBtn").addEventListener("click", scrollToTop);
+
+
