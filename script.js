c1f2609cf20dcf49f6f14c8273505fe5372fb98a
Jawad Khalil <iamjawadkhalil@gmail.com>
2024-08-13 08:45:51 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
-
 function toggleMenu() {
-  const menu = document.getElementById('menu');
-  menu.classList.toggle('active');
+  const menu = document.getElementById("menu");
+  menu.classList.toggle("active");
 }
 
 window.addEventListener("scroll", function () {
-    var scrollBtn = document.getElementById("scrollToTopBtn");
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        scrollBtn.style.display = "block";
-    } else {
-        scrollBtn.style.display = "none";
-    }
+  var scrollBtn = document.getElementById("scrollToTopBtn");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollBtn.classList.add("show");
+  } else {
+    scrollBtn.classList.remove("show");
+  }
 });
 
 function scrollToTop() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
-document.getElementById("scrollToTopBtn").addEventListener("click", scrollToTop);
+document
+  .getElementById("scrollToTopBtn")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    scrollToTop();
+  });
 
-// Add this script at the end of your HTML or in a separate JS file
-document.addEventListener("DOMContentLoaded", function() {
-    const currentYear = new Date().getFullYear();
-    document.getElementById("footerYear").textContent = currentYear;
+document.addEventListener("DOMContentLoaded", function () {
+  const currentYear = new Date().getFullYear();
+  document.getElementById("footerYear").textContent = currentYear;
 });
